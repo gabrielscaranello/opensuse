d1f8bb5276c9f62974ea7b930fc8c0f9edd98849
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2025-07-09 19:39:28 -0300

    fix: flatpak script and packages

--- scripts/install-flatpak-packages.sh
+++ scripts/install-flatpak-packages.sh
@@ -5,10 +5,10 @@ PACKAGES=$(tr '\n' ' ' <"${PWD}/flatpak-packages")
 
 echo "Installing flatpak packages..."
 echo "Enabling Flathub..."
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-flatpak override --user --filesystem=/usr/share/fonts
+sudo flatpak override --filesystem=/usr/share/fonts
 
 echo "Installing packages..."
-echo "$PACKAGES" | xargs flatpak install flathub -y
+echo "$PACKAGES" | xargs sudo flatpak install flathub -y
 echo "Flatpak packages installed."
