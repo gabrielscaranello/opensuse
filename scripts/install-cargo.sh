f9cfc6e13e0568eac326bbe7c1b83b80c495eeac
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-02-22 14:20:35 -0300

    feat: update packages and scripts

--- scripts/install-cargo.sh
+++ scripts/install-cargo.sh
@@ -1,15 +1,20 @@
 #! /bin/bash
 
-echo "Installing cargo..."
+PWD=$(pwd)
+PACKAGES=$(tr '\n' ' ' <"${PWD}/cargo-packages")
 
+echo "Installing cargo..."
 echo "Installing rustup if not installed..."
 
-if ! command -v cargo &> /dev/null; then
+if ! command -v cargo &>/dev/null; then
   sudo zypper install -y rustup
 fi
 
 echo "Installing Rust stable..."
-
 rustup install stable
+
+echo "Installing cargo packages..."
+echo "$PACKAGES" | xargs cargo install
+echo "Cargo packages installed."
 
 echo "Cargo installed..."
--- scripts/install-go.sh
+++ scripts/install-go.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-VERSION=1.25.4
+VERSION=1.26.0
 FILENAME=go${VERSION}.linux-amd64.tar.gz
 DOWNLOAD_URL=https://go.dev/dl/${FILENAME}
 DOWNLOAD_FILE=/tmp/${FILENAME}
